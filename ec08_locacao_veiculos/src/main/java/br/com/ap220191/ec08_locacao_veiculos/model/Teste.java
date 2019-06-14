f86143e73bf2a1a1bb41a0e1f250e5cac4065b3f
IanChaves <ianchaves10@hotmail.com>
2019-06-14 16:51:17 -0300

    Criando Classe Locação!

--- ec08_locacao_veiculos/src/main/java/br/com/ap220191/ec08_locacao_veiculos/model/Teste.java
+++ ec08_locacao_veiculos/src/main/java/br/com/ap220191/ec08_locacao_veiculos/model/Teste.java
@@ -1,14 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package br.com.ap220191.ec01_faculdade.model;
-
-/**
- *
- * @author gilmario
- */
+package br.com.ap220191.ec08_locacao_veiculos.model;
 public class Teste {
     public static void main(String args[]){
         System.out.println("Hello World!");
---
+++ ec08_locacao_veiculos/src/main/java/br/com/ap220191/ec08_locacao_veiculos/util/Locacao.java
@@ -0,0 +1,22 @@
+package br.com.ap220191.ec08_locacao_veiculos.util;
+import java.util.Date;
+public class Locacao {
+    private Date dataLocacao;
+    private Date dataDevolucao;
+    private double quilometragemLocacao;
+    private double quilometragemDevolucao;
+    
+    public boolean consultarDisponibilidade(String placa){
+        return true;
+    }
+    public void realizarLocacao(String placa, Date dataLocacao){
+        
+    }
+    public double calcularValor(Date dataLocacao, Date dataDevolucao){
+        return 00.1;
+    }
+    
+    
+    
+    
+}
