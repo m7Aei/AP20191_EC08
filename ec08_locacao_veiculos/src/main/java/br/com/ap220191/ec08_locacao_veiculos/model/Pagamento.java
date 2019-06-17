1bea264496647e59c252e96b7bbbc0359b5baf43
Dhiuly <dhiulysoaresds@gmail.com>
2019-06-17 10:25:44 -0300

    classe Pagamento

--- ec08_locacao_veiculos/src/main/java/br/com/ap220191/ec08_locacao_veiculos/model/Pagamento.java
+++ ec08_locacao_veiculos/src/main/java/br/com/ap220191/ec08_locacao_veiculos/model/Pagamento.java
@@ -1,5 +1,16 @@
 package br.com.ap220191.ec08_locacao_veiculos.model;
 
 public class Pagamento {
-
+	private int parcelamento;
+	private float valor;
+	
+	public void efetuarPagamento() {
+		
+	}
+	public void consultarSerasa() {
+		
+	}
+	public void consultarInadimplencia() {
+		
+	}
 }
