c2b8fc4dc037434e829f92e135dca70c9eaa0ef2
Dhiuly <dhiulysoaresds@gmail.com>
2019-06-17 10:03:59 -0300

    método mostraOpcao em classe enum FormaPagamento

--- ec08_locacao_veiculos/src/main/java/br/com/ap220191/ec08_locacao_veiculos/model/FormaPagamento.java
+++ ec08_locacao_veiculos/src/main/java/br/com/ap220191/ec08_locacao_veiculos/model/FormaPagamento.java
@@ -1,7 +1,7 @@
 package br.com.ap220191.ec08_locacao_veiculos.model;
 
 public enum FormaPagamento {
-	DINHEIRO(1), CARTAO(2), CHEQUE(3);
+	DINHEIRO(1), CARTAOCREDITO1X(2), CARTAOCREDITO2X(3), CARTAOCREDITO3X(4), CARTAODEBITO(3), CHEQUE(4);
 	
 	private final int pgto;
 	FormaPagamento(int forma) {
@@ -10,4 +10,25 @@ public enum FormaPagamento {
 	public int getPgto() {
 		return pgto;
 	}
+	
+	public void mostraOpcao(FormaPagamento pgto){
+        if(pgto == FormaPagamento.DINHEIRO){
+            System.out.println("Pagamento em dinheiro.");
+        }
+        if(pgto == FormaPagamento.CARTAOCREDITO1X){
+            System.out.println("Pagamento no cartão de crédito.\nCrédito à vista.");
+        }
+        if(pgto == FormaPagamento.CARTAOCREDITO2X){
+            System.out.println("Pagamento no cartão de crédito.\nParcelado 2x.");
+        }
+        if(pgto == FormaPagamento.CARTAOCREDITO3X){
+            System.out.println("Pagamento no cartão de crédito.\nParcelado 3x.");
+        }
+        if(pgto == FormaPagamento.CARTAODEBITO){
+            System.out.println("Pagamento no cartão de débito");
+        }
+        if(pgto == FormaPagamento.CHEQUE){
+            System.out.println("Pagamento em cheque.");
+        }
+    }
 }
