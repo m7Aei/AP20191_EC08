df85b78a05086591c2d8ea0190a10255a8e26579
natalie9 <natalietolentino9@hotmail.com>
2019-07-15 20:37:41 -0300

    Merge newBranch

--- ec08_locacao_veiculos/src/main/java/br/com/ap220191/ec08_locacao_veiculos/controller/MenuCadastroMotoristaController.java
+++ ec08_locacao_veiculos/src/main/java/br/com/ap220191/ec08_locacao_veiculos/controller/MenuCadastroMotoristaController.java
@@ -1,41 +1,5 @@
 package br.com.ap220191.ec08_locacao_veiculos.controller;
 
-import br.com.ap220191.ec08_locacao_veiculos.model.Motorista;
-import br.com.ap220191.ec08_locacao_veiculos.model.dao.Conexao;
-import br.com.ap220191.ec08_locacao_veiculos.model.dao.MotoristaDAO;
-import br.com.ap220191.ec08_locacao_veiculos.view.MenuCadastroMotoristas;
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
-/**
- *
- * @author gattc
- */
 public class MenuCadastroMotoristaController {
-
-    private MenuCadastroMotoristas view;
-
-    public MenuCadastroMotoristaController(MenuCadastroMotoristas view) {
-        this.view = view;
-    }
-
-<<<<<<< HEAD
-   
-=======
-    public void salvarMotorista() {
-
-//        Motorista motorista = new Motorista(view.getjTextFieldNomeMotorista().getText(), view.getjTextFieldMatriculaMotorista().getText(), view.getjComboBoxTipoHabilitacao(), view.getjTextFieldTempoMotorista().getText());
-
-        try {
-            Connection conexao = new Conexao().getConnection();
-            MotoristaDAO motoristaDao = new MotoristaDAO(conexao);
-//            motoristaDao.insert(motorista);
-        } catch (SQLException ex) {
-            Logger.getLogger(MenuCadastroMotoristas.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
->>>>>>> 64365f6fa1e4cbd44f28505aca283eb848e4b1f6
 
 }
