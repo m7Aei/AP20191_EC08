1e0f3829f61e783aa4a7faee4715cfb77415d8d7
natalie9 <natalietolentino9@hotmail.com>
2019-07-15 20:50:06 -0300

    Merge newBranch

--- ec08_locacao_veiculos/src/main/java/br/com/ap220191/ec08_locacao_veiculos/controller/MenuCadastroAutomovelController.java
+++ ec08_locacao_veiculos/src/main/java/br/com/ap220191/ec08_locacao_veiculos/controller/MenuCadastroAutomovelController.java
@@ -5,36 +5,6 @@
  */
 package br.com.ap220191.ec08_locacao_veiculos.controller;
 
-import br.com.ap220191.ec08_locacao_veiculos.model.Automovel;
-import br.com.ap220191.ec08_locacao_veiculos.model.dao.AutomovelDAO;
-import br.com.ap220191.ec08_locacao_veiculos.model.dao.Conexao;
-import br.com.ap220191.ec08_locacao_veiculos.view.MenuCadastroVeiculos;
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 public class MenuCadastroAutomovelController {
-      private MenuCadastroVeiculos view;
 
-    public MenuCadastroAutomovelController(MenuCadastroVeiculos view) {
-        this.view = view;
-    }
-
-
-<<<<<<< HEAD
-    
-=======
-//        Automovel automovel = new Automovel(view.getjTextFieldPlaca().getText(),view.getjComboBoxTipoVeículo());
-
-        try {
-            Connection conexao = new Conexao().getConnection();
-            AutomovelDAO automovelDao = new AutomovelDAO(conexao);
-//            automovelDao.insert(automovel);
-        } catch (SQLException ex) {
-            Logger.getLogger(MenuCadastroVeiculos.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
->>>>>>> 64365f6fa1e4cbd44f28505aca283eb848e4b1f6
-    
 }
