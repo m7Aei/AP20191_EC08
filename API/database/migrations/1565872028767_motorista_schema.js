e8e89d2a3f76d3167c8bd3a4ec4547b0fc9c369b
m7Aei <mateuspm2015@hotmail.com>
2019-08-15 09:36:06 -0300

    Adicionando a tabela dos motoristas

---
+++ API/database/migrations/1565872028767_motorista_schema.js
@@ -0,0 +1,29 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class MotoristaSchema extends Schema {
+  up () {
+    this.create('motoristas', table => {
+      table.increments()
+      table.string('nome', 255).notNullable()
+      table
+        .string('matricula', 255)
+        .notNullable()
+        .unique()
+      table
+        .string('cnh', 12)
+        .notNullable()
+        .unique()
+      table.integer('tempoEmpresa').notNullable()
+      table.timestamps()
+    })
+  }
+
+  down () {
+    this.drop('motoristas')
+  }
+}
+
+module.exports = MotoristaSchema
